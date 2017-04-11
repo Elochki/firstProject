72bf945b9d6606583bb5ab1e6ea52036aaec8801
Lena <zyamileva@ukr.net>
2017-04-11 23:45:44 +0300

    first commit

---
+++ GitTestProject/src/main/java/com/HelloWord.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by Elena on 11.04.2017.
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
